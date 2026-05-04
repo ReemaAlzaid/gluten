e627ce15246758eb9f5e40d961fe8cff201b2afa
Reema Alzaid <reema-alzaid@outlook.com>
2026-05-04 18:23:06 +0300

    [VL] Refactor Gluten S3 registration

--- cpp/velox/filesystem/GlutenS3FileSystem.cc
+++ cpp/velox/filesystem/GlutenS3FileSystem.cc
@@ -17,20 +17,10 @@
 
 #include "filesystem/GlutenS3FileSystem.h"
 
-#include <folly/Synchronized.h>
-#include <glog/logging.h>
-
-#include <functional>
 #include <memory>
-#include <optional>
-#include <string>
-#include <unordered_map>
+#include <utility>
 
-#include "velox/common/base/Exceptions.h"
-#include "velox/common/file/FileSystems.h"
-#include "velox/connectors/hive/storage_adapters/s3fs/S3Config.h"
-#include "velox/connectors/hive/storage_adapters/s3fs/S3Util.h"
-#include "velox/dwio/common/FileSink.h"
+#include "velox/common/file/File.h"
 
 namespace gluten {
 
@@ -39,91 +29,10 @@ namespace filesystems = facebook::velox::filesystems;
 
 namespace {
 
-using FileSystemMap = folly::Synchronized<
-    std::unordered_map<std::string, std::shared_ptr<filesystems::FileSystem>>>;
-using FileSystemGenerator = std::function<std::shared_ptr<filesystems::FileSystem>(
-    std::shared_ptr<const velox::config::ConfigBase>,
-    std::string_view)>;
-using FileSinkGenerator =
-    std::function<std::unique_ptr<velox::dwio::common::FileSink>(
-        const std::string&,
-        const velox::dwio::common::FileSink::Options&)>;
-
-FileSystemMap& glutenS3FileSystems() {
-  static FileSystemMap instances;
-  return instances;
-}
-
-filesystems::CacheKeyFn glutenS3CacheKeyFunc;
-
-std::shared_ptr<filesystems::FileSystem> glutenS3FileSystemGenerator(
-    std::shared_ptr<const velox::config::ConfigBase> properties,
-    std::string_view s3Path) {
-  std::string cacheKey;
-  std::string bucketName;
-  std::string key;
-  filesystems::getBucketAndKeyFromPath(
-      filesystems::getPath(s3Path), bucketName, key);
-  if (glutenS3CacheKeyFunc) {
-    cacheKey = glutenS3CacheKeyFunc(properties, s3Path);
-  } else {
-    cacheKey = filesystems::S3Config::cacheKey(bucketName, properties);
-  }
-
-  auto fs = glutenS3FileSystems().withRLock(
-      [&](auto& instanceMap) -> std::shared_ptr<filesystems::FileSystem> {
-        auto iterator = instanceMap.find(cacheKey);
-        if (iterator != instanceMap.end()) {
-          LOG(WARNING) << "[GlutenS3FileSystem] cache hit for S3 path: "
-                       << s3Path;
-          return iterator->second;
-        }
-        return nullptr;
-      });
-  if (fs != nullptr) {
-    return fs;
-  }
-
-  return glutenS3FileSystems().withWLock(
-      [&](auto& instanceMap) -> std::shared_ptr<filesystems::FileSystem> {
-        auto iterator = instanceMap.find(cacheKey);
-        if (iterator != instanceMap.end()) {
-          LOG(WARNING) << "[GlutenS3FileSystem] cache hit for S3 path: "
-                       << s3Path;
-          return iterator->second;
-        }
-
-        auto logLevel = properties->get(
-            filesystems::S3Config::kS3LogLevel, std::string("FATAL"));
-        std::optional<std::string> logLocation =
-            static_cast<std::optional<std::string>>(
-                properties->get<std::string>(
-                    filesystems::S3Config::kS3LogLocation));
-        filesystems::initializeS3(logLevel, logLocation);
-        LOG(WARNING) << "[GlutenS3FileSystem] creating filesystem for bucket: "
-                     << bucketName;
-        auto fs = std::make_shared<GlutenS3FileSystem>(bucketName, properties);
-        instanceMap.insert({cacheKey, fs});
-        return fs;
-      });
-}
-
-std::unique_ptr<velox::dwio::common::FileSink> glutenS3WriteFileSinkGenerator(
-    const std::string& fileURI,
-    const velox::dwio::common::FileSink::Options& options) {
-  if (filesystems::isS3File(fileURI)) {
-    LOG(WARNING) << "[GlutenS3FileSystem] creating WriteFileSink for path: "
-                 << fileURI;
-    auto fileSystem =
-        filesystems::getFileSystem(fileURI, options.connectorProperties);
-    return std::make_unique<velox::dwio::common::WriteFileSink>(
-        fileSystem->openFileForWrite(fileURI, {{}, options.pool, std::nullopt}),
-        fileURI,
-        options.metricLogger,
-        options.stats,
-        options.fileSystemStats);
-  }
-  return nullptr;
+std::shared_ptr<filesystems::FileSystem> glutenS3FileSystemFactory(
+    std::string_view bucketName,
+    std::shared_ptr<const velox::config::ConfigBase> config) {
+  return std::make_shared<GlutenS3FileSystem>(bucketName, config);
 }
 
 } // namespace
@@ -131,39 +40,16 @@ std::unique_ptr<velox::dwio::common::FileSink> glutenS3WriteFileSinkGenerator(
 std::unique_ptr<velox::WriteFile> GlutenS3FileSystem::openFileForWrite(
     std::string_view s3Path,
     const filesystems::FileOptions& options) {
-  LOG(WARNING) << "[GlutenS3FileSystem] openFileForWrite delegating to Velox "
-                  "S3 for path: "
-               << s3Path;
   return filesystems::S3FileSystem::openFileForWrite(s3Path, options);
 }
 
 void registerGlutenS3FileSystem(filesystems::CacheKeyFn cacheKeyFunc) {
-  glutenS3FileSystems().withWLock([&](auto& instanceMap) {
-    if (instanceMap.empty()) {
-      LOG(WARNING) << "[GlutenS3FileSystem] registering";
-      glutenS3CacheKeyFunc = cacheKeyFunc;
-      filesystems::registerFileSystem(
-          filesystems::isS3File,
-          FileSystemGenerator(glutenS3FileSystemGenerator));
-      velox::dwio::common::FileSink::registerFactory(
-          FileSinkGenerator(glutenS3WriteFileSinkGenerator));
-    }
-  });
+  filesystems::registerS3FileSystem(
+      std::move(cacheKeyFunc), glutenS3FileSystemFactory);
 }
 
 void finalizeGlutenS3FileSystem() {
-  LOG(WARNING) << "[GlutenS3FileSystem] finalizing";
-  bool singleUseCount = true;
-  glutenS3FileSystems().withWLock([&](auto& instanceMap) {
-    for (const auto& entry : instanceMap) {
-      singleUseCount &= (entry.second.use_count() == 1);
-    }
-    VELOX_CHECK(
-        singleUseCount, "Cannot finalize GlutenS3FileSystem while in use");
-    instanceMap.clear();
-  });
-
-  filesystems::finalizeS3();
+  filesystems::finalizeS3FileSystem();
 }
 
 } // namespace gluten
